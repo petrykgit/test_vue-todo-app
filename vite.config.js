fa417cd1eac6f6f15b9324cda248890557a48b61
Ievgen Petryk <jakeshad123@gmail.com>
2025-12-15 15:40:11 +0200

    add base url

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/test_vue-todo-app/',
   plugins: [
     vue(),
     vueDevTools(),
